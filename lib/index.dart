27ab890ac095a3299b1a3c419f9ec10f044a99ed
nayliazizan <149150399+nayliazizan@users.noreply.github.com>
2023-11-12 23:56:53 +0800

    Add files via upload

---
+++ lib/index.dart
@@ -0,0 +1,96 @@
+// Export pages
+export '/main_pages/home_page/home_page_widget.dart' show HomePageWidget;
+export '/signin_page/signin_page_widget.dart' show SigninPageWidget;
+export '/main_pages/profile_page/profile_page_widget.dart'
+    show ProfilePageWidget;
+export '/child_profile_page/edit_profile/edit_profile_widget.dart'
+    show EditProfileWidget;
+export '/child_profile_page/view_diagnosis/view_diagnosis_widget.dart'
+    show ViewDiagnosisWidget;
+export '/main_pages/manage_appointments/manage_appointments_widget.dart'
+    show ManageAppointmentsWidget;
+export '/child_profile_page/view_consult_notes/view_consult_notes_widget.dart'
+    show ViewConsultNotesWidget;
+export '/child_manage_appointment/appointment_details/appointment_details_widget.dart'
+    show AppointmentDetailsWidget;
+export '/admin_pages/admin_dashboard/admin_dashboard_widget.dart'
+    show AdminDashboardWidget;
+export '/admin_pages/patient_registration/patient_registration_widget.dart'
+    show PatientRegistrationWidget;
+export '/admin_pages/resources_list/resources_list_widget.dart'
+    show ResourcesListWidget;
+export '/admin_pages/patient_list/patient_list_widget.dart'
+    show PatientListWidget;
+export '/admin_pages/resource_details/resource_details_widget.dart'
+    show ResourceDetailsWidget;
+export '/main_pages/view_resources/view_resources_widget.dart'
+    show ViewResourcesWidget;
+export '/diagnosis_admin/diagnosis_details/diagnosis_details_widget.dart'
+    show DiagnosisDetailsWidget;
+export '/diagnosis_admin/diagnoses_list_1/diagnoses_list1_widget.dart'
+    show DiagnosesList1Widget;
+export '/diagnosis_admin/diagnoses_list_2/diagnoses_list2_widget.dart'
+    show DiagnosesList2Widget;
+export '/test_results_admin/test_result_list_1/test_result_list1_widget.dart'
+    show TestResultList1Widget;
+export '/test_results_admin/test_result_list_2/test_result_list2_widget.dart'
+    show TestResultList2Widget;
+export '/doctor_pages/iii_test_result_details/iii_test_result_details_widget.dart'
+    show IiiTestResultDetailsWidget;
+export '/child_profile_page/view_diagnosis_details/view_diagnosis_details_widget.dart'
+    show ViewDiagnosisDetailsWidget;
+export '/child_profile_page/view_test_result/view_test_result_widget.dart'
+    show ViewTestResultWidget;
+export '/child_profile_page/view_test_result_details/view_test_result_details_widget.dart'
+    show ViewTestResultDetailsWidget;
+export '/child_profile_page/view_consult_note_details/view_consult_note_details_widget.dart'
+    show ViewConsultNoteDetailsWidget;
+export '/concern_by_patient_for_doctor/concerns_list_1/concerns_list1_widget.dart'
+    show ConcernsList1Widget;
+export '/concern_by_patient_for_doctor/concerns_list_2/concerns_list2_widget.dart'
+    show ConcernsList2Widget;
+export '/concern_by_patient_for_doctor/concern_details/concern_details_widget.dart'
+    show ConcernDetailsWidget;
+export '/doctor_pages/a_doctor_dashboard/a_doctor_dashboard_widget.dart'
+    show ADoctorDashboardWidget;
+export '/doctor_pages/b_list_of_patients/b_list_of_patients_widget.dart'
+    show BListOfPatientsWidget;
+export '/doctor_pages/c_one_patient/c_one_patient_widget.dart'
+    show COnePatientWidget;
+export '/doctor_pages/i_notes/i_notes_widget.dart' show INotesWidget;
+export '/doctor_pages/ii_diagnoses/ii_diagnoses_widget.dart'
+    show IiDiagnosesWidget;
+export '/doctor_pages/iii_test_result/iii_test_result_widget.dart'
+    show IiiTestResultWidget;
+export '/doctor_pages/iv_symptoms/iv_symptoms_widget.dart'
+    show IvSymptomsWidget;
+export '/doctor_pages/v_concerns/v_concerns_widget.dart' show VConcernsWidget;
+export '/doctor_pages/i_notes_details/i_notes_details_widget.dart'
+    show INotesDetailsWidget;
+export '/doctor_pages/ii_diagnoses_details/ii_diagnoses_details_widget.dart'
+    show IiDiagnosesDetailsWidget;
+export '/test_results_admin/test_result_details/test_result_details_widget.dart'
+    show TestResultDetailsWidget;
+export '/doctor_pages/v_concerns_details/v_concerns_details_widget.dart'
+    show VConcernsDetailsWidget;
+export '/main_pages/manage_symptoms/manage_symptoms_widget.dart'
+    show ManageSymptomsWidget;
+export '/child_manage_symptoms/symptoms_add/symptoms_add_widget.dart'
+    show SymptomsAddWidget;
+export '/child_manage_symptoms/symptoms_bar_chart/symptoms_bar_chart_widget.dart'
+    show SymptomsBarChartWidget;
+export '/child_manage_symptoms/symptom_details/symptom_details_widget.dart'
+    show SymptomDetailsWidget;
+export '/manage_medications/medic_list_1/medic_list1_widget.dart'
+    show MedicList1Widget;
+export '/manage_medications/medic_list_2/medic_list2_widget.dart'
+    show MedicList2Widget;
+export '/manage_medications/medic_details/medic_details_widget.dart'
+    show MedicDetailsWidget;
+export '/child_profile_page/view_medications/view_medications_widget.dart'
+    show ViewMedicationsWidget;
+export '/child_profile_page/view_medications_details/view_medications_details_widget.dart'
+    show ViewMedicationsDetailsWidget;
+export '/doctor_pages/vi_medic/vi_medic_widget.dart' show ViMedicWidget;
+export '/doctor_pages/vi_medic_details/vi_medic_details_widget.dart'
+    show ViMedicDetailsWidget;
